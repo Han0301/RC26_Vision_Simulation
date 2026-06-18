c7e5563e13a466732f3d0f1d9b3c88bdfe39bc72
Han0301 <2987374677@qq.com>
2026-06-18 20:50:17 +0800

    Delete world_ws13 directory

--- world_ws13/build/3dto2d/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/h/RC2026/world_ws13/src/3dto2d
-# Build directory: /home/h/RC2026/world_ws13/build/3dto2d
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
