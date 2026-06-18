da3d8246215ec843a4113144b2b38f882d8590ab
Han0301 <2987374677@qq.com>
2026-06-18 20:04:56 +0800

    Delete world_ws13 directory

--- world_ws13/build/zwei/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/h/RC2026/world_ws13/src/zwei
-# Build directory: /home/h/RC2026/world_ws13/build/zwei
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
