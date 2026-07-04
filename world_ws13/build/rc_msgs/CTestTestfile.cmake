548e0422fb763cd7c0a81c425e79211c3da1b113
Han Zhang <2987374677@qq.com>
2026-07-04 11:03:11 +0800

    Delete world_ws13 directory

--- world_ws13/build/rc_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/h/RC2026/world_ws13/src/rc_msgs
-# Build directory: /home/h/RC2026/world_ws13/build/rc_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
